038bfeb63555d0eb2a7d1862040ec28478ee1638
Al <al@eartoearoak.com>
2014-05-20 20:50:16 +0100

    Copyright stuff

--- Location Bridge/Server.cs
+++ Location Bridge/Server.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/*
+ Location Bridge
+
+ http://eartoearoak.com/software/location-bridge
+
+ Copyright 2014 Al Brown
+
+ A simple server that provides NMEA sentences over TCP from the
+ Windows location sensors.
+
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, or (at your option)
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
